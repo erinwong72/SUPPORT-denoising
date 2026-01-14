562c1e5fb532248b3a041d980d53c366bc91bc7c
Erin <udonoodlesoup@gmail.com>
2026-01-14 13:09:59 -0500

    paths changed in inference.sh

--- model/inference.sh
+++ model/inference.sh
@@ -4,8 +4,9 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate SUPPORT
+cd /home/knswift/SUPPORT/SUPPORT-denoising/model/
 
-export RAW="/mnt/fanlab/Labmembers/Xingyu/CCK-GtACR-BTSP/CCK-GtACR-BTSP-w02/2025-12-23_VR-V-Blue/" # where output from support is saved
+export RAW="/mnt/fanlab/Labmembers/Kohl/CCK/cck-inhDSI-w08/2026-01-13-VR-V_blue/" # where output from support is saved
 export LOG_DIR="/home/knswift/SUPPORT/results/logs/inference/"
 export MODEL="/home/knswift/SUPPORT/results/saved_models/cck-gevi_post/model_49.pth"
 export GPU_ID=0
