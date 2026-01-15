16df736e06416a1361bf807d663f2dfbc7349e4e
Erin Wong <erinwong@dhcp-10-29-195-129.dyn.MIT.EDU>
2026-01-15 15:58:22 -0500

    updated inference and models

--- preprocessing/run_inference.sh
+++ preprocessing/run_inference.sh
@@ -5,18 +5,21 @@ source ~/miniconda3/etc/profile.d/conda.sh
 USER=$1
 CONFIG=$2
 
-ROOT=/shared/ml_inference
+ROOT=/home/$USER/
 
+# activate conda environment
 if conda env list | awk '{print $1}' | grep -Fxq "SUPPORT"; then
     conda activate SUPPORT
     echo "Activated SUPPORT environment"
 else
     # check if support denoising repository is cloned
-    if [ ! -d "/home/$USER/SUPPORT-denoising"]; then
+    if [ ! -d "$ROOT/SUPPORT-denoising"]; then
         # then create conda directory using .yml file
-        git clone https://github.com/erinwong72/SUPPORT-denoising.git /home/$USER/
+        git clone https://github.com/erinwong72/SUPPORT-denoising.git $ROOT
     fi
-    conda env create -f /home/$USER/SUPPORT-denoising/model/support_model.yml
+    conda env create -f $ROOT/SUPPORT-denoising/model/support_model.yml -n SUPPORT
+    conda activate SUPPORT
+    echo "Created and activated SUPPORT environment"
 
 
 LOGDIR="$SESSION_DIR/logs"
