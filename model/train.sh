441687e454088468e474ca6da1332092e2cd6a2d
Erin <udonoodlesoup@gmail.com>
2026-01-22 09:46:15 -0500

    update'

--- model/train.sh
+++ model/train.sh
@@ -4,12 +4,25 @@ conda activate SUPPORT
 
 ## YOU ONLY NEED TO CHANGE THESE VARIABLES
 USER=knswift
-export MODEL_NAME="cck-gevi_post" # change the name to what you want your model to be called (e.g. basket cell)
-export TRAINING_DATA="/mnt/fanlab/Labmembers/Kohl/Support/cck-gevi/dataset/train/post-motion" # make sure to add /mnt/fanlab in front of labmembers
+export MODEL_NAME="pyramidal_cell" # change the name to what you want your model to be called (e.g. basket cell)
+export TRAINING_DATA="/mnt/fanlab/Computer Code/SUPPORT-denoising/datasets/PC/train" # make sure to add /mnt/fanlab in front of labmembers
 # make sure this is the directory where the source code for SUPPORT is (copied from Roshni's setup) 
 export src_dir="/home/$USER/SUPPORT/"
 
 ## NO NEED TO CHANGE BELOW THIS LINE
-cd $src_dir
-nohup bash -c 'set -e; python -m src.train --exp_name "$MODEL_NAME" --noisy_data "$TRAINING_DATA" --n_epochs 50 --auto_patch_size --is_folder' \
-> /dev/null 2>&1 &
+cd "$src_dir"
+
+nohup bash -c '
+  set -ex
+  which python
+  python --version
+  pwd
+  python -m src.train \
+    --exp_name "$1" \
+    --noisy_data "$2" \
+    --n_epochs 50 \
+    --auto_patch_size \
+    --is_folder
+' bash "$MODEL_NAME" "$TRAINING_DATA" \
+> /home/knswift/SUPPORT/results/logs/train/pyramidal_cell.log 2>&1 &
+echo "Training started for model $MODEL_NAME. Check train.log for progress."
